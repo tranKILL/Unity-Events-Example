5aa895924a1e62ef44d900ad4e27622e6abc80d0
tranKILL.be <pit190@gmail.com>
2023-10-14 01:42:05 +0200

    VFX animation selon temps propre ou répétée si courte, replacement caméra

--- Assets/Script/PlayAllPrefabInFolderAndSubfolder.cs
+++ Assets/Script/PlayAllPrefabInFolderAndSubfolder.cs
@@ -1,12 +1,13 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
 namespace NJ_Event
 {
-    using System.Collections;
-    using TMPro;
-    using UnityEngine;
-    
     public class PlayAllPrefabInFolderAndSubfolder : MonoBehaviour
     {
         public string resourcesPrefabPath = "Mirza Beig/Particle Systems/Ultimate VFX/Prefabs/";
+        public Vector3 decalPrefab = new Vector3(0, 0, -2);
         public float loopTime = 2.2f;
         public float oneshotTime = 2.0f;
 
@@ -26,19 +27,52 @@ namespace NJ_Event
 
         private IEnumerator PlayPrefabs()
         {
-            float time;
             foreach (GameObject prefab in prefabs)
             {
                 // Create a new instance of the prefab.
-                GameObject vfx = Instantiate(prefab, transform.position, transform.rotation);
+                GameObject vfx = Instantiate(prefab, transform.position + decalPrefab, transform.rotation);
                 vfxName.text = vfx.name.Replace("pf_vfx-ult_demo_psys_", "").Replace("(Clone)", "");
-                // Start playing the vfx.
-                vfx.SetActive(true);
-                time = loopTime;
-                yield return new WaitForSeconds(time);
+
+                float animationDuration = loopTime;
+                var particleSystem = vfx.GetComponent<ParticleSystem>();
+                Animation animation = null;
+                if (particleSystem != null)
+                {
+                    var mainModule = particleSystem.main;
+                    animationDuration = mainModule.duration;
+                }
+                else
+                {
+                    animation = vfx.GetComponent<Animation>();
+                    if (animation != null)
+                    {
+                        animationDuration = animation.clip.length;
+                    }
+                }
+
+                // Play the animation multiple times if it's shorter than loopTime.
+                int repetitions = Mathf.CeilToInt(loopTime / animationDuration);
+                //if (repetitions == 1) repetitions++;
+//Debug.Log("animationDuration:" + animationDuration + " - loopTime:" + loopTime + " - repetitions:" + repetitions);
+                for (int i = 0; i < repetitions; i++)
+                {
+                    if (particleSystem != null)
+                    {
+                        particleSystem.Play();
+                    }
+                    else if (animation != null)
+                    {
+                        animation.Play();
+                    }
+
+                    // Wait for the animation to complete.
+                    yield return new WaitForSeconds(animationDuration);
+                }
+
                 Destroy(vfx);
             }
         }
+
         private void ShuffleArray(GameObject[] array)
         {
             int n = array.Length;
